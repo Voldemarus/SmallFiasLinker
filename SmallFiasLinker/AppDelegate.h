43034ee7925c114bcae6d331e7d184cc12e9e461
Vladimir Vodolazkiy <Vladimir.Vodolazkiy@asteros.ru>
2015-12-24 11:20:20 +0300

    Initial Commit

---
+++ SmallFiasLinker/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  SmallFiasLinker
+//
+//  Created by Водолазкий В.В. on 24.12.15.
+//  Copyright © 2015 Geomatix Laboratoriess S.R.O. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+
+@end
+
---
+++ SmallFiasLinker/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  SmallFiasLinker
+//
+//  Created by Водолазкий В.В. on 24.12.15.
+//  Copyright © 2015 Geomatix Laboratoriess S.R.O. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+	return NSApplicationMain(argc, argv);
+}
